b6143f4de2ddc4fb4b16146e96145df2491b9fdc
christopherross <christopherross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-06 15:56:59 +0000

    - documentation updates - WordPress 3.5.1 compatibility checks
    git-svn-id: https://plugins.svn.wordpress.org/stop-pinging-yourself-for-wordpress/trunk@737094 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- stop-pinging-yourself-for-wordpress.php
+++ stop-pinging-yourself-for-wordpress.php
@@ -1,41 +1,27 @@
 <?php
 /*
 Plugin Name: Stop Pinging Yourself for WordPress
-Plugin URI: http://thisismyurl.com/downloads/wordpress/plugins/stop-pinging-yourself-for-wordpress/
+Plugin URI: http://thisismyurl.com/plugins/stop-pinging-yourself-for-wordpress/
 Description: Stops a WordPress blog from pinging itself with pingbacks
 Author: christopherross
 Version: 1.0.0
-Author URI: http://thisismyurl.com
+Author URI: http://thisismyurl.com/
 */
 
 
-/*
-/--------------------------------------------------------------------\
-|                                                                    |
-| License: GPL                                                       |
-|                                                                    |
-| Copyright (C) 2011, c.bavota, Christopher Ross		    		 |
-| http://thisismyurl.com, http://bavotasan.com						 |
-| All rights reserved.                                               |
-|                                                                    |
-| This program is free software; you can redistribute it and/or      |
-| modify it under the terms of the GNU General Public License        |
-| as published by the Free Software Foundation; either version 2     |
-| of the License, or (at your option) any later version.             |
-|                                                                    |
-| This program is distributed in the hope that it will be useful,    |
-| but WITHOUT ANY WARRANTY; without even the implied warranty of     |
-| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      |
-| GNU General Public License for more details.                       |
-|                                                                    |
-| You should have received a copy of the GNU General Public License  |
-| along with this program; if not, write to the                      |
-| Free Software Foundation, Inc.                                     |
-| 51 Franklin Street, Fifth Floor                                    |
-| Boston, MA  02110-1301, USA                                        |
-|                                                                    |
-\--------------------------------------------------------------------/
-*/
+/**
+ * Stop Pinging Yourself for WordPress core file
+ *
+ * This file contains all the logic required for the plugin
+ *
+ * @link		http://wordpress.org/extend/plugins/stop-pinging-yourself-for-wordpress/
+ *
+ * @package 		Stop Pinging Yourself for WordPress
+ * @copyright		Copyright (c) 2008, Chrsitopher Ross
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, v2 (or newer)
+ *
+ * @since 		Stop Pinging Yourself for WordPress 1.0
+ */
 
 function thisismyurl_no_self_ping( $links ) {
 
